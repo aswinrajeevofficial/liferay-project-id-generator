2513bf649f00c4c171f9396b30fc778b6bc2b7a9
Aswin Rajeev <aswin.rajeev@liferay.com>
2021-10-01 10:46:00 +0100

    Removed Generex library and added RxGen

--- src/main/java/idgenerator/IdGenerator.java
+++ src/main/java/idgenerator/IdGenerator.java
@@ -1,26 +1,13 @@
 package idgenerator;
 
-import java.util.List;
-
-import com.mifmif.common.regex.Generex;
-import com.mifmif.common.regex.util.Iterator;
+import com.github.curiousoddman.rgxgen.RgxGen;
 
 public class IdGenerator {
-	
-	public static void main(String[] args) {
-		Generex generex = new Generex("[a-z][2-9][a-z][2-9]");
-
-		String randomStr = generex.random();
-		System.out.println(randomStr);
-
-		/*
-		Using a Generx Iterator to get all matched strings to the above regular
-		expression and printing them one by one
-		List<String> matchedStrs = generex.getAllMatchedStrings();
-		Iterator iterator = generex.iterator();
-		while (iterator.hasNext()) {
-			System.out.print(iterator.next() + " ");
+	public static void main(String[] args){
+		for(int i = 0; i < 100; i++) {
+			RgxGen rgxGen = new RgxGen("[^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9][^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9]$");   
+	        String s = rgxGen.generate();                                        
+	        System.out.println(s);
 		}
-		*/
-	}
+    }
 }
