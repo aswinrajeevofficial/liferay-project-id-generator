67edf379be9af09ed9839faa38d829d12217f7ee
Aswin Rajeev <aswin.rajeev@liferay.com>
2021-10-01 10:48:27 +0100

    Updated README

--- src/main/java/idgenerator/IdGenerator.java
+++ src/main/java/idgenerator/IdGenerator.java
@@ -4,10 +4,8 @@ import com.github.curiousoddman.rgxgen.RgxGen;
 
 public class IdGenerator {
 	public static void main(String[] args){
-		for(int i = 0; i < 100; i++) {
-			RgxGen rgxGen = new RgxGen("[^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9][^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9]$");   
-	        String s = rgxGen.generate();                                        
-	        System.out.println(s);
-		}
+		RgxGen rgxGen = new RgxGen("[^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9][^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9]$");   
+        String s = rgxGen.generate();                                        
+        System.out.println(s);
     }
 }
