a30e83973af42d0c99563984c0a2040d97e41261
Aswin Rajeev <aswin.rajeev@liferay.com>
2023-02-21 19:18:06 +0000

    added random UUID generation

--- src/main/java/idgenerator/IdGenerator.java
+++ src/main/java/idgenerator/IdGenerator.java
@@ -1,11 +1,16 @@
 package idgenerator;
 
 import com.github.curiousoddman.rgxgen.RgxGen;
-
+import java.util.UUID;
 public class IdGenerator {
 	public static void main(String[] args){
 		RgxGen rgxGen = new RgxGen("[^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9][^{}$&+,:;=?@#|'-`<>.-_\\s^*()%!~\\\"A-Z0-9oli][2-9]$");   
         String s = rgxGen.generate();                                        
-        System.out.println("Here is your random Project ID: " + s);
+        System.out.println("Random Project ID: " + s);
+
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+
+        System.out.println("Random UUID: " + uuidAsString);
     }
 }
